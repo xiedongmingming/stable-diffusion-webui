0fe94e1602a81e5b08e8c92f113bfa7cb2b1a9d2
xiedongmingming <986617424@qq.com>
2023-05-31 23:54:40 +0800

    优化

--- launch.py
+++ launch.py
@@ -63,7 +63,7 @@ def commit_hash():
         return stored_commit_hash
 
     try:
-        stored_commit_hash = run(f"{git} rev-parse HEAD").strip() # 'git rev-parse HEAD'
+        stored_commit_hash = run(f"{git} rev-parse HEAD").strip()  # 'git rev-parse HEAD'
     except Exception:
         stored_commit_hash = "<none>"
 
@@ -83,15 +83,15 @@ Error code: {result.returncode}""")
 
         return ""
 
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, env=os.environ if custom_env is None else custom_env)
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                            env=os.environ if custom_env is None else custom_env)
 
     if result.returncode != 0:
-
         message = f"""{errdesc or 'Error running command'}.
 Command: {command}
 Error code: {result.returncode}
-stdout: {result.stdout.decode(encoding="utf8", errors="ignore") if len(result.stdout)>0 else '<empty>'}
-stderr: {result.stderr.decode(encoding="utf8", errors="ignore") if len(result.stderr)>0 else '<empty>'}
+stdout: {result.stdout.decode(encoding="utf8", errors="ignore") if len(result.stdout) > 0 else '<empty>'}
+stderr: {result.stderr.decode(encoding="utf8", errors="ignore") if len(result.stderr) > 0 else '<empty>'}
 """
         raise RuntimeError(message)
 
@@ -125,7 +125,8 @@ def run_pip(command, desc=None, live=False):
         return
 
     index_url_line = f' --index-url {index_url}' if index_url != '' else ''
-    return run(f'"{python}" -m pip {command} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}", live=live)
+    return run(f'"{python}" -m pip {command} --prefer-binary{index_url_line}', desc=f"Installing {desc}",
+               errdesc=f"Couldn't install {desc}", live=live)
 
 
 def check_run_python(code):
@@ -139,12 +140,14 @@ def git_clone(url, dir, name, commithash=None):
         if commithash is None:
             return
 
-        current_hash = run(f'"{git}" -C "{dir}" rev-parse HEAD', None, f"Couldn't determine {name}'s hash: {commithash}").strip()
+        current_hash = run(f'"{git}" -C "{dir}" rev-parse HEAD', None,
+                           f"Couldn't determine {name}'s hash: {commithash}").strip()
         if current_hash == commithash:
             return
 
         run(f'"{git}" -C "{dir}" fetch', f"Fetching updates for {name}...", f"Couldn't fetch {name}")
-        run(f'"{git}" -C "{dir}" checkout {commithash}', f"Checking out commit for {name} with hash: {commithash}...", f"Couldn't checkout commit {commithash} for {name}")
+        run(f'"{git}" -C "{dir}" checkout {commithash}', f"Checking out commit for {name} with hash: {commithash}...",
+            f"Couldn't checkout commit {commithash} for {name}")
         return
 
     run(f'"{git}" clone "{url}" "{dir}"', f"Cloning {name} into {dir}...", f"Couldn't clone {name}")
@@ -166,7 +169,8 @@ def git_pull_recursive(dir):
 def version_check(commit):
     try:
         import requests
-        commits = requests.get('https://api.github.com/repos/AUTOMATIC1111/stable-diffusion-webui/branches/master').json()
+        commits = requests.get(
+            'https://api.github.com/repos/AUTOMATIC1111/stable-diffusion-webui/branches/master').json()
         if commit != "<none>" and commits['commit']['sha'] != commit:
             print("--------------------------------------------------------")
             print("| You are not up to date with the most recent release. |")
@@ -189,7 +193,8 @@ def run_extension_installer(extension_dir):
         env = os.environ.copy()
         env['PYTHONPATH'] = os.path.abspath(".")
 
-        print(run(f'"{python}" "{path_installer}"', errdesc=f"Error running install.py for extension {extension_dir}", custom_env=env))
+        print(run(f'"{python}" "{path_installer}"', errdesc=f"Error running install.py for extension {extension_dir}",
+                  custom_env=env))
     except Exception as e:
         print(e, file=sys.stderr)
 
@@ -222,27 +227,80 @@ def run_extensions_installers(settings_file):
 
 
 def prepare_environment():
-    torch_command = os.environ.get('TORCH_COMMAND', "pip install torch==2.0.0 torchvision==0.15.1 --extra-index-url https://download.pytorch.org/whl/cu118")
-    requirements_file = os.environ.get('REQS_FILE', "requirements_versions.txt")
+    #
+    torch_command = os.environ.get(
+        'TORCH_COMMAND',
+        "pip install torch==2.0.0 torchvision==0.15.1 --extra-index-url https://download.pytorch.org/whl/cu118"
+    )
 
-    xformers_package = os.environ.get('XFORMERS_PACKAGE', 'xformers==0.0.17')
-    gfpgan_package = os.environ.get('GFPGAN_PACKAGE', "git+https://ghproxy.com/github.com/TencentARC/GFPGAN.git@8d2447a2d918f8eba5a4a01463fd48e45126a379")
-    clip_package = os.environ.get('CLIP_PACKAGE', "git+https://ghproxy.com/github.com/openai/CLIP.git@d50d76daa670286dd6cacf3bcd80b5e4823fc8e1")
-    openclip_package = os.environ.get('OPENCLIP_PACKAGE', "git+https://ghproxy.com/github.com/mlfoundations/open_clip.git@bb6e834e9c70d9c27d0dc3ecedeebeaeb1ffad6b")
+    requirements_file = os.environ.get(
+        'REQS_FILE',
+        "requirements_versions.txt"
+    )
 
-    stable_diffusion_repo = os.environ.get('STABLE_DIFFUSION_REPO', "https://ghproxy.com/github.com/Stability-AI/stablediffusion.git")
-    taming_transformers_repo = os.environ.get('TAMING_TRANSFORMERS_REPO', "https://ghproxy.com/github.com/CompVis/taming-transformers.git")
-    k_diffusion_repo = os.environ.get('K_DIFFUSION_REPO', 'https://ghproxy.com/github.com/crowsonkb/k-diffusion.git')
-    codeformer_repo = os.environ.get('CODEFORMER_REPO', 'https://ghproxy.com/github.com/sczhou/CodeFormer.git')
-    blip_repo = os.environ.get('BLIP_REPO', 'https://ghproxy.com/github.com/salesforce/BLIP.git')
+    xformers_package = os.environ.get(
+        'XFORMERS_PACKAGE',
+        'xformers==0.0.17'
+    )
+
+    gfpgan_package = os.environ.get(
+        'GFPGAN_PACKAGE',
+        "git+https://ghproxy.com/github.com/TencentARC/GFPGAN.git@8d2447a2d918f8eba5a4a01463fd48e45126a379"
+    )
+    clip_package = os.environ.get(
+        'CLIP_PACKAGE',
+        "git+https://ghproxy.com/github.com/openai/CLIP.git@d50d76daa670286dd6cacf3bcd80b5e4823fc8e1"
+    )
+    openclip_package = os.environ.get(
+        'OPENCLIP_PACKAGE',
+        "git+https://ghproxy.com/github.com/mlfoundations/open_clip.git@bb6e834e9c70d9c27d0dc3ecedeebeaeb1ffad6b"
+    )
+
+    stable_diffusion_repo = os.environ.get(
+        'STABLE_DIFFUSION_REPO',
+        "https://ghproxy.com/github.com/Stability-AI/stablediffusion.git"
+    )
+    taming_transformers_repo = os.environ.get(
+        'TAMING_TRANSFORMERS_REPO',
+        "https://ghproxy.com/github.com/CompVis/taming-transformers.git"
+    )
+    k_diffusion_repo = os.environ.get(
+        'K_DIFFUSION_REPO',
+        'https://ghproxy.com/github.com/crowsonkb/k-diffusion.git'
+    )
+    codeformer_repo = os.environ.get(
+        'CODEFORMER_REPO',
+        'https://ghproxy.com/github.com/sczhou/CodeFormer.git'
+    )
+    blip_repo = os.environ.get(
+        'BLIP_REPO',
+        'https://ghproxy.com/github.com/salesforce/BLIP.git'
+    )
+
     # 提交记录
-    stable_diffusion_commit_hash = os.environ.get('STABLE_DIFFUSION_COMMIT_HASH', "cf1d67a6fd5ea1aa600c4df58e5b47da45f6bdbf")
-    taming_transformers_commit_hash = os.environ.get('TAMING_TRANSFORMERS_COMMIT_HASH', "24268930bf1dce879235a7fddd0b2355b84d7ea6")
-    k_diffusion_commit_hash = os.environ.get('K_DIFFUSION_COMMIT_HASH', "5b3af030dd83e0297272d861c19477735d0317ec")
-    codeformer_commit_hash = os.environ.get('CODEFORMER_COMMIT_HASH', "c5b4593074ba6214284d6acd5f1719b6c5d739af")
-    blip_commit_hash = os.environ.get('BLIP_COMMIT_HASH', "48211a1594f1321b00f14c9f7a5b4813144b2fb9")
+    stable_diffusion_commit_hash = os.environ.get(
+        'STABLE_DIFFUSION_COMMIT_HASH',
+        "cf1d67a6fd5ea1aa600c4df58e5b47da45f6bdbf"
+    )
+    taming_transformers_commit_hash = os.environ.get(
+        'TAMING_TRANSFORMERS_COMMIT_HASH',
+        "24268930bf1dce879235a7fddd0b2355b84d7ea6"
+    )
+    k_diffusion_commit_hash = os.environ.get(
+        'K_DIFFUSION_COMMIT_HASH',
+        "5b3af030dd83e0297272d861c19477735d0317ec"
+    )
+    codeformer_commit_hash = os.environ.get(
+        'CODEFORMER_COMMIT_HASH',
+        "c5b4593074ba6214284d6acd5f1719b6c5d739af"
+    )
+    blip_commit_hash = os.environ.get(
+        'BLIP_COMMIT_HASH',
+        "48211a1594f1321b00f14c9f7a5b4813144b2fb9"
+    )
 
     if not args.skip_python_version_check:
+        #
         check_python_version()
 
     commit = commit_hash()
@@ -251,103 +309,192 @@ def prepare_environment():
     print(f"Commit hash: {commit}")
 
     if args.reinstall_torch or not is_installed("torch") or not is_installed("torchvision"):
+        #
         run(f'"{python}" -m {torch_command}', "Installing torch and torchvision", "Couldn't install torch", live=True)
 
     if not args.skip_torch_cuda_test:
-        run_python("import torch; assert torch.cuda.is_available(), 'Torch is not able to use GPU; add --skip-torch-cuda-test to COMMANDLINE_ARGS variable to disable this check'")
+        #
+        run_python(
+            "import torch; assert torch.cuda.is_available(), 'Torch is not able to use GPU; add --skip-torch-cuda-test to COMMANDLINE_ARGS variable to disable this check'"
+        )
 
     if not is_installed("gfpgan"):
+        #
         run_pip(f"install {gfpgan_package}", "gfpgan")
 
     if not is_installed("clip"):
+        #
         run_pip(f"install {clip_package}", "clip")
 
     if not is_installed("open_clip"):
+        #
         run_pip(f"install {openclip_package}", "open_clip")
 
     if (not is_installed("xformers") or args.reinstall_xformers) and args.xformers:
+
         if platform.system() == "Windows":
+
             if platform.python_version().startswith("3.10"):
+
                 run_pip(f"install -U -I --no-deps {xformers_package}", "xformers", live=True)
+
             else:
+
                 print("Installation of xformers is not supported in this version of Python.")
-                print("You can also check this and build manually: https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Xformers#building-xformers-on-windows-by-duckness")
+                print(
+                    "You can also check this and build manually: https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Xformers#building-xformers-on-windows-by-duckness"
+                )
+
                 if not is_installed("xformers"):
+                    #
                     exit(0)
+
         elif platform.system() == "Linux":
+            #
             run_pip(f"install {xformers_package}", "xformers")
 
     if not is_installed("pyngrok") and args.ngrok:
+        #
         run_pip("install pyngrok", "ngrok")
 
     os.makedirs(os.path.join(script_path, dir_repos), exist_ok=True)
 
-    git_clone(stable_diffusion_repo, repo_dir('stable-diffusion-stability-ai'), "Stable Diffusion", stable_diffusion_commit_hash)
-    git_clone(taming_transformers_repo, repo_dir('taming-transformers'), "Taming Transformers", taming_transformers_commit_hash)
-    git_clone(k_diffusion_repo, repo_dir('k-diffusion'), "K-diffusion", k_diffusion_commit_hash)
-    git_clone(codeformer_repo, repo_dir('CodeFormer'), "CodeFormer", codeformer_commit_hash)
-    git_clone(blip_repo, repo_dir('BLIP'), "BLIP", blip_commit_hash)
+    git_clone(
+        stable_diffusion_repo,
+        repo_dir('stable-diffusion-stability-ai'),
+        "Stable Diffusion",
+        stable_diffusion_commit_hash
+    )
+    git_clone(
+        taming_transformers_repo,
+        repo_dir('taming-transformers'),
+        "Taming Transformers",
+        taming_transformers_commit_hash
+    )
+    git_clone(
+        k_diffusion_repo,
+        repo_dir('k-diffusion'),
+        "K-diffusion",
+        k_diffusion_commit_hash
+    )
+    git_clone(
+        codeformer_repo,
+        repo_dir('CodeFormer'),
+        "CodeFormer",
+        codeformer_commit_hash
+    )
+    git_clone(
+        blip_repo,
+        repo_dir('BLIP'),
+        "BLIP",
+        blip_commit_hash
+    )
 
     if not is_installed("lpips"):
-        run_pip(f"install -r \"{os.path.join(repo_dir('CodeFormer'), 'requirements.txt')}\"", "requirements for CodeFormer")
+        #
+        run_pip(
+            f"install -r \"{os.path.join(repo_dir('CodeFormer'), 'requirements.txt')}\"",
+            "requirements for CodeFormer"
+        )
 
     if not os.path.isfile(requirements_file):
+        #
         requirements_file = os.path.join(script_path, requirements_file)
-    run_pip(f"install -r \"{requirements_file}\"", "requirements")
-    # '"...python.exe" -m pip install -r "requirements_versions.txt" --prefer-binary'
+
+    run_pip(  # '"...python.exe" -m pip install -r "requirements_versions.txt" --prefer-binary'
+        f"install -r \"{requirements_file}\"",
+        "requirements"
+    )
+
     run_extensions_installers(settings_file=args.ui_settings_file)
 
     if args.update_check:
+        #
         version_check(commit)
 
     if args.update_all_extensions:
+        #
         git_pull_recursive(extensions_dir)
-    
+
     if "--exit" in sys.argv:
+        #
         print("Exiting because of --exit argument")
+
         exit(0)
 
     if args.tests and not args.no_tests:
+        #
         exitcode = tests(args.tests)
+
         exit(exitcode)
 
 
 def tests(test_dir):
+    #
     if "--api" not in sys.argv:
+        #
         sys.argv.append("--api")
+
     if "--ckpt" not in sys.argv:
+        #
         sys.argv.append("--ckpt")
         sys.argv.append(os.path.join(script_path, "test/test_files/empty.pt"))
+
     if "--skip-torch-cuda-test" not in sys.argv:
+        #
         sys.argv.append("--skip-torch-cuda-test")
+
     if "--disable-nan-check" not in sys.argv:
+        #
         sys.argv.append("--disable-nan-check")
+
     if "--no-tests" not in sys.argv:
+        #
         sys.argv.append("--no-tests")
 
     print(f"Launching Web UI in another process for testing with arguments: {' '.join(sys.argv[1:])}")
 
     os.environ['COMMANDLINE_ARGS'] = ""
-    with open(os.path.join(script_path, 'test/stdout.txt'), "w", encoding="utf8") as stdout, open(os.path.join(script_path, 'test/stderr.txt'), "w", encoding="utf8") as stderr:
+
+    with open(
+            os.path.join(script_path, 'test/stdout.txt'),
+            "w",
+            encoding="utf8"
+    ) as stdout, open(
+        os.path.join(script_path, 'test/stderr.txt'),
+        "w",
+        encoding="utf8"
+    ) as stderr:
+
         proc = subprocess.Popen([sys.executable, *sys.argv], stdout=stdout, stderr=stderr)
 
     import test.server_poll
+
     exitcode = test.server_poll.run_tests(proc, test_dir)
 
     print(f"Stopping Web UI process with id {proc.pid}")
+
     proc.kill()
+
     return exitcode
 
 
 def start():
+    #
     print(f"Launching {'API server' if '--nowebui' in sys.argv else 'Web UI'} with arguments: {' '.join(sys.argv[1:])}")
+
     import webui
+
     if '--nowebui' in sys.argv:
         webui.api_only()
     else:
-        webui.webui()
+        webui.webui()  # 可以同时开启API模式
 
 
 if __name__ == "__main__":
+    #
+    # 准备环境
+    #
     prepare_environment()
+
     start()
