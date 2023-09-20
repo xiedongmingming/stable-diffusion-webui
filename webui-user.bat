82c5d6a16dcb159771a174c6dd84cd2c11852cb0
xiedongmingming <986617424@qq.com>
2023-09-20 23:10:45 +0800

    启动脚本优化

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=G:\workspace\github\xiedongmingming\stable-diffusion-webui\venv\Scripts\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --autolaunch --api --listen --enable-insecure-extension-access
 
 call webui.bat
